0eb2268d632210a45f473a8e3332b25ac3b02cc4
nmsoikat <nursaykat@gmail.com>
2022-01-30 21:36:19 +0600

    use multer

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const multer = require('multer');
+const path = require('path');
 
 // ROUTER
-const userRouter = require('./routes/userRouter')
-const authRouter = require('./routes/authRouter')
-const postRouter = require('./routes/postRouter')
+const userRouter = require('./routes/userRouter');
+const authRouter = require('./routes/authRouter');
+const postRouter = require('./routes/postRouter');
 
 // APP
 const app = express();
@@ -25,19 +27,47 @@ mongoose.connect(process.env.MONGODB_URL, {
 })
 
 
+// if made any request for /images 
+// don't make any request just go to /public/images directory
+app.use('/images', express.static(path.join(__dirname, "/public/images")))
+// app.use('/images', express.static(`${__dirname}/public/images`))
+
+
 // MIDDLEWARE
 app.use(express.json()) // body parser
 app.use(helmet())
 app.use(morgan('common'));
 
-app.get('/api/v1', (req, res) => {
-  res.send('API is running')
+
+// MULTER | FILE UPLOAD
+//destination
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  }
 })
+
+const upload = multer({ storage });
+app.post('/api/v1/upload', upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully.");
+  } catch (err) {
+    console.log(err);
+  }
+})
+
 
 // USE ROUTE
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/posts', postRouter);
+
+app.get('/api/v1', (req, res) => {
+  res.send('API is running')
+})
 
 // LISTEN
 const PORT = process.env.PORT || 5000;
