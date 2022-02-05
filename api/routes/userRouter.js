031fd545610c382d1330f1ffa86f036ebb7f61bb
nmsoikat <nursaykat@gmail.com>
2022-02-05 22:54:29 +0600

    follower

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const { find } = require('../models/User');
 const User = require('../models/User')
 
 const router = require('express').Router();
@@ -94,4 +95,26 @@ router.put('/:id/unfollow', async (req, res) => {
   }
 })
 
+// FRIENDS AND FOLLOWER
+router.get('/friends/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId)
+      })
+    )
+
+    let friendList = [];
+    friends.map(friend => {
+      const { _id, username, profilePicture } = friend;
+      friendList.push({ _id, username, profilePicture })
+    })
+
+    res.status(200).json(friendList);
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 module.exports = router;
