b522d0ec049e9b6200c5c1f277bbc2eec40e9470
nmsoikat <nursaykat@gmail.com>
2022-02-11 15:26:53 +0600

    follow btn design

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -22,7 +22,7 @@ export default function Feed({ username }) {
 
   return <div className='feed'>
     <div className="feed-wrapper">
-      {username === user.username &&  <Share />}
+      {username ? username === user.username && <Share /> : <Share />}
       {
         posts.map(p => (
           <Post key={p._id} post={p} />
--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,27 +1,26 @@
 import './rightbar.css';
 import OnlineFriend from '../onlineFriend/OnlineFriend';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import { Add, Remove } from '@mui/icons-material';
 
 export default function Rightbar({ user }) {
+
   const [friends, setFriends] = useState([]);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user: currentUser } = useContext(AuthContext);
+  const [followed, setFollowed] = useState(false)
 
   useEffect(() => {
-    const getFriends = async () => {
-      try {
-        const friendList = await axios.get('/users/friends/' + user._id)
-        console.log(friendList.data);
-        setFriends(friendList.data)
-      } catch (err) {
-        console.log(err);
-      }
-    }
+    setFollowed(currentUser.followings.includes())
+  }, [currentUser, user.id])
 
-    getFriends();
-  }, [user._id])
+  const handleClick = () => {
+
+  }
 
   const HomeRightbar = () => {
     return (
@@ -34,18 +33,37 @@ export default function Rightbar({ user }) {
         </div>
         <img className="rightbar-ad" src="/assets/ad.png" alt="" />
         <h4 className="rightbar-title">Online Friends</h4>
-        <ul className="rightbar-friend-list">
+        {/* <ul className="rightbar-friend-list">
           {user.map(u => (
             <OnlineFriend key={u.id} user={u} />
           ))}
-        </ul>
+        </ul> */}
       </>
     )
   }
 
   const ProfileRightbar = () => {
+    useEffect(() => {
+      const getFriends = async () => {
+        try {
+          const friendList = await axios.get('/users/friends/' + user._id)
+          setFriends(friendList.data)
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      getFriends();
+    }, [user._id])
+
     return (
       <>
+        {currentUser.username !== user.username && (
+          <button className='rightbar-follow-btn' onClick={handleClick}>
+            {followed ? 'Unfollow' : 'Follow'}
+            {followed ? <Remove /> : <Add />}
+          </button>
+        )}
         <h4 className="rightbar-title">User Information</h4>
         <div className="rightbar-info">
           <div className="rightbar-info-item">
@@ -66,11 +84,11 @@ export default function Rightbar({ user }) {
         <div className="rightbar-followings">
           {
             friends.map(friend => (
-              <Link to={`/profile/${friend.username}`} style={{textDecoration: 'none'}}>
-              <div className="rightbar-following">
-                <img className='rightbar-following-img' src={`${(friend.profilePicture && PF + friend.profilePicture) || PF + '/person/noAvatar.png'}`} alt="" />
-                <span className="rightbar-following-name">{friend.username}</span>
-              </div>
+              <Link to={`/profile/${friend.username}`} style={{ textDecoration: 'none' }}>
+                <div className="rightbar-following">
+                  <img className='rightbar-following-img' src={`${(friend.profilePicture && PF + friend.profilePicture) || PF + '/person/noAvatar.png'}`} alt="" />
+                  <span className="rightbar-following-name">{friend.username}</span>
+                </div>
               </Link>
             ))
           }
