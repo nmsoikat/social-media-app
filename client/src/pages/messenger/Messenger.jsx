e8826b45029d3b01cf3dd470b1b3a5cdd3a62658
nmsoikat <nursaykat@gmail.com>
2022-02-24 23:54:30 +0600

    new message

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -12,16 +12,16 @@ function Messenger() {
   //select user from left bar
   const [currentChat, setCurrentChat] = useState(null)
 
-  //find conversation according current-conversation
+  //find conversation and set according current-chat
   const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState("");
 
-
-  const { user } = useContext(AuthContext)
+  const { user:currentUser } = useContext(AuthContext)
 
   useEffect(() => {
     const getConversation = async () => {
       try {
-        const { data } = await axios.get('/conversation/' + user._id)
+        const { data } = await axios.get('/conversation/' + currentUser._id)
         setConversations(data)
       } catch (err) {
         console.log(err);
@@ -43,6 +43,26 @@ function Messenger() {
 
     getMessage()
   }, [currentChat])
+
+  const newMessageHandler = async(e) => {
+    e.preventDefault()
+    const message = {
+      sender: currentUser._id,
+      text: newMessage,
+      conversationId: currentChat._id
+    }
+
+    try {
+      //same to db
+      const {data} = await axios.post("/message", message);//return created data
+
+      setMessages([...messages, data]) // frontend dom update
+
+      setNewMessage(""); //reset text box
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <>
       <Topbar />
@@ -50,9 +70,9 @@ function Messenger() {
         <div className="chat-menu">
           <div className="chat-menu-wrapper">
             <input type="text" placeholder="search for friends" className="chat-menu-input" />
-            {conversations.map(c =>
-              <div onClick={() => setCurrentChat(c)}>
-                <Conversation conversation={c} currentUser={user} />
+            {conversations.map((c,index) =>
+              <div key={index} onClick={() => setCurrentChat(c)}>
+                <Conversation conversation={c} currentUser={currentUser} />
               </div>
             )}
           </div>
@@ -63,11 +83,11 @@ function Messenger() {
               currentChat ?
                 (<>
                   <div className="chat-box-top">
-                    {messages.map(m => <Message message={m} own={m.sender === user._id} currentUser={user} />)}
+                    {messages.map((m,index) => <Message key={index} message={m} own={m.sender === currentUser._id} currentUser={currentUser} />)}
                   </div>
                   <div className="chat-box-bottom">
-                    <textarea cols="30" rows="10" className="chat-message-input" placeholder="write something..."></textarea>
-                    <button className="chat-submit-btn">Send</button>
+                    <textarea cols="30" rows="10" className="chat-message-input" placeholder="write something..." value={newMessage} onChange={(e)=> setNewMessage(e.target.value)}></textarea>
+                    <button className="chat-submit-btn" onClick={newMessageHandler}>Send</button>
                   </div>
                 </>) : (<p className="no-conversation-text">Open a conversation to start chat</p>)
             }
